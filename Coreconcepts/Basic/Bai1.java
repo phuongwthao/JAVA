6287f58d9a85d33b577c7959e14259786a09f764
phuongwthao <phuongthaoluu2401@gmail.com>
2024-08-14 10:01:16 +0700

    1

---
+++ Coreconcepts/Basic/Bai1.java
@@ -0,0 +1,4 @@
+public class Bai1 {
+    public static void main(String[] args) {
+    }
+}
---
+++ Coreconcepts/src/test/java/com/example/coreconcepts/CoreconceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coreconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreconceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
